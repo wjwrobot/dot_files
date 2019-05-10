94a7f614358b9f8c22daebab4b309cb8779b4cb7
wjwrobot <wjwsuper123@gmail.com>
2019-05-11 02:15:13 +0800

    spacemacs config

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,3 +1,5 @@
 (("default" . ((user-emacs-directory . "~/.emacs.d")))
  ("doom" . ((user-emacs-directory . "~/Git/doom")
-           (env . (("DOOMDIR" . "~/.doom.d"))))))
+           (env . (("DOOMDIR" . "~/.doom.d")))))
+ ("spacemacs" . ((user-emacs-directory . "~/Git/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/.spacemacs.d"))))))
