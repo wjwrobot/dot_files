e648cbc4bfc6c30c87911d980245f9ef7a4a4791
wjwrobot <wjwsuper123@gmail.com>
2019-05-11 02:22:12 +0800

    enable sshd.socket daemon

--- .init.sh
+++ .init.sh
@@ -2,6 +2,7 @@
 # my private init scripy
 
 sudo systemctl start privoxy.service		#privoxy
+sudo systemctl start sshd.socket            #ssh server
 sudo ssr start	#ssr
 ibus-daemon --xim &		 #ibus
 sudo wifi-menu  #wifi
