87089098b9322b51c2a276683befc2f628b3500c
Rick <wjwsuper123@gmail.com>
2020-08-11 13:45:10 +0000

    modify v2ray server deploy file

--- misc/v2ray_server_deploy.sh
+++ misc/v2ray_server_deploy.sh
@@ -64,185 +64,185 @@ function install() {
 
 function v2ray_config() {
     cat > /etc/v2ray/config.json <<-EOF
-    {
-        "inbounds": [
-            {
-                "protocol": "vmess",
-                "listen": "127.0.0.1",
-                "port": 8686,
-                "settings": {
-                    "clients": [
-                        {
-                            "id": "$UUID"
-                        }
-                    ]
-                },
-                "streamSettings": {
-                    "network": "ws",
-                    "wsSettings": {
-                        "path": "/$rand_path"
+{
+    "inbounds": [
+        {
+            "protocol": "vmess",
+            "listen": "127.0.0.1",
+            "port": 8686,
+            "settings": {
+                "clients": [
+                    {
+                        "id": "$UUID"
                     }
-                }
-            }
-        ],
-        "outbounds": [
-            {
-                "protocol": "freedom",
-                "settings": {},
-                "tag": "direct"
+                ]
             },
-            {
-                "protocol": "blackhole",
-                "settings": {},
-                "tag": "blocked"
-            }
-        ],
-        "routing": {
-            "domainStrategy": "IPOnDemand",
-            "rules": [
-                {
-                    "domain": [],
-                    "type": "field",
-                    "outboundTag": "blocked"
+            "streamSettings": {
+                "network": "ws",
+                "wsSettings": {
+                    "path": "/$rand_path"
                 }
-            ]
+            }
         }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {},
+            "tag": "direct"
+        },
+        {
+            "protocol": "blackhole",
+            "settings": {},
+            "tag": "blocked"
+        }
+    ],
+    "routing": {
+        "domainStrategy": "IPOnDemand",
+        "rules": [
+            {
+                "domain": [],
+                "type": "field",
+                "outboundTag": "blocked"
+            }
+        ]
     }
+}
 EOF
 }
 
 function nginx_config() {
     cat > /etc/nginx/conf.d/default.conf <<-EOF
-    server {
-        ### 1:
-        server_name $domain;
+server {
+    ### 1:
+    server_name $domain;
 
-        listen 80 reuseport fastopen=10;
-        rewrite ^(.*) https://\$server_name\$1 permanent;
-        if (\$request_method  !~ ^(POST|GET)\$) { return  501; }
-        autoindex off;
-        server_tokens off;
-    }
+    listen 80 reuseport fastopen=10;
+    rewrite ^(.*) https://\$server_name\$1 permanent;
+    if (\$request_method  !~ ^(POST|GET)\$) { return  501; }
+    autoindex off;
+    server_tokens off;
+}
 
-    server {
-        ### 2:
-        ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
+server {
+    ### 2:
+    ssl_certificate /etc/letsencrypt/live/$domain/fullchain.pem;
 
-        ### 3:
-        ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;
+    ### 3:
+    ssl_certificate_key /etc/letsencrypt/live/$domain/privkey.pem;
 
-        ### 4:
-        location /$rand_path
-        {
-            proxy_pass http://127.0.0.1:8686;
-            proxy_redirect off;
-            proxy_http_version 1.1;
-            proxy_set_header Upgrade \$http_upgrade;
-            proxy_set_header Connection "upgrade";
-            proxy_set_header Host \$host;
-
-            sendfile on;
-            tcp_nopush on;
-            tcp_nodelay on;
-            keepalive_requests 10000;
-            keepalive_timeout 2h;
-            proxy_buffering off;
-        }
-
-        listen 443 ssl reuseport fastopen=10;
-        server_name \$server_name;
-        charset utf-8;
+    ### 4:
+    location /$rand_path
+    {
+        proxy_pass http://127.0.0.1:8686;
+        proxy_redirect off;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
+        proxy_set_header Host \$host;
 
         sendfile on;
         tcp_nopush on;
         tcp_nodelay on;
         keepalive_requests 10000;
         keepalive_timeout 2h;
-
-        ssl_protocols TLSv1.2 TLSv1.3;
-        ssl_ciphers TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-256-GCM-SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
-        ssl_ecdh_curve secp384r1;
-        ssl_prefer_server_ciphers off;
-
-        ssl_session_cache shared:SSL:60m;
-        ssl_session_timeout 1d;
-        ssl_session_tickets off;
-        ssl_stapling on;
-        ssl_stapling_verify on;
-        resolver 8.8.8.8 8.8.4.4 valid=300s;
-        resolver_timeout 10s;
-
-        if (\$request_method  !~ ^(POST|GET)\$) { return 501; }
-        add_header X-Frame-Options DENY;
-        add_header X-XSS-Protection "1; mode=block";
-        add_header X-Content-Type-Options nosniff;
-        add_header Strict-Transport-Security max-age=31536000 always;
-        autoindex off;
-        server_tokens off;
-
-        index index.html index.htm  index.php;
-        location ~ .*\.(js|jpg|JPG|jpeg|JPEG|css|bmp|gif|GIF|png)\$ { access_log off; }
-        location / { index index.html; }
+        proxy_buffering off;
     }
+
+    listen 443 ssl reuseport fastopen=10;
+    server_name \$server_name;
+    charset utf-8;
+
+    sendfile on;
+    tcp_nopush on;
+    tcp_nodelay on;
+    keepalive_requests 10000;
+    keepalive_timeout 2h;
+
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_ciphers TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-256-GCM-SHA384:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
+    ssl_ecdh_curve secp384r1;
+    ssl_prefer_server_ciphers off;
+
+    ssl_session_cache shared:SSL:60m;
+    ssl_session_timeout 1d;
+    ssl_session_tickets off;
+    ssl_stapling on;
+    ssl_stapling_verify on;
+    resolver 8.8.8.8 8.8.4.4 valid=300s;
+    resolver_timeout 10s;
+
+    if (\$request_method  !~ ^(POST|GET)\$) { return 501; }
+    add_header X-Frame-Options DENY;
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Content-Type-Options nosniff;
+    add_header Strict-Transport-Security max-age=31536000 always;
+    autoindex off;
+    server_tokens off;
+
+    index index.html index.htm  index.php;
+    location ~ .*\.(js|jpg|JPG|jpeg|JPEG|css|bmp|gif|GIF|png)\$ { access_log off; }
+    location / { index index.html; }
+}
 EOF
 }
 
 function generate_client_file() {
     cat > /usr/share/nginx/html/config.json <<-EOF
+{
+"inbounds": [
     {
-    "inbounds": [
-        {
-        "listen": "127.0.0.1",
-        "port": 1080, // 监听端口
-        "protocol": "socks", // 入口协议为 SOCKS 5
-        "sniffing": {
-            "enabled": true,
-            "destOverride": [
-            "http",
-            "tls"
-            ]
-        },
-        "settings": {
-            "auth": "noauth" //socks的认证设置，noauth 代表不认证，由于 socks 通常在客户端使用，所以这里不认证
-        }
-        }
-    ],
-    "outbounds": [
-        {
-        "protocol": "vmess", // 出口协议
-        "settings": {
-            "vnext": [
-            {
-                "address": "$domain", // 服务器地址，请修改为你自己的服务器 IP 或域名
-                "port": 443, // 服务器端口
-                "users": [
-                {
-                    "id": "$UUID", // 用户 ID，必须与服务器端配置相同
-                    "alterId": 0, // 此处的值也应当与服务器相同
-                    "security": "auto"
-                }
-                ]
-            }
-            ]
-        },
-        "streamSettings": {
-            "network": "ws",
-            "security": "tls",
-            "tlssettings": {
-            "allowInsecure": true,
-            "serverName": "$domain"
-            },
-            "wssettings": {
-            "connectionReuse": true,
-            "headers": {
-                "Host": "$domain"
-            },
-            "path": "/$rand_path"
-            }
-        }
-        }
-    ]
+    "listen": "127.0.0.1",
+    "port": 1080, // 监听端口
+    "protocol": "socks", // 入口协议为 SOCKS 5
+    "sniffing": {
+        "enabled": true,
+        "destOverride": [
+        "http",
+        "tls"
+        ]
+    },
+    "settings": {
+        "auth": "noauth" //socks的认证设置，noauth 代表不认证，由于 socks 通常在客户端使用，所以这里不认证
     }
+    }
+],
+"outbounds": [
+    {
+    "protocol": "vmess", // 出口协议
+    "settings": {
+        "vnext": [
+        {
+            "address": "$domain", // 服务器地址，请修改为你自己的服务器 IP 或域名
+            "port": 443, // 服务器端口
+            "users": [
+            {
+                "id": "$UUID", // 用户 ID，必须与服务器端配置相同
+                "alterId": 0, // 此处的值也应当与服务器相同
+                "security": "auto"
+            }
+            ]
+        }
+        ]
+    },
+    "streamSettings": {
+        "network": "ws",
+        "security": "tls",
+        "tlssettings": {
+        "allowInsecure": true,
+        "serverName": "$domain"
+        },
+        "wssettings": {
+        "connectionReuse": true,
+        "headers": {
+            "Host": "$domain"
+        },
+        "path": "/$rand_path"
+        }
+    }
+    }
+]
+}
 EOF
 }
 
@@ -271,7 +271,6 @@ function main() {
     generate_client_file
     #====================
     mv /usr/share/nginx/html/config.json /usr/share/nginx/html/${rand_config_file_path}/config.json
-    green "客户端配置文件放置在http://${domain}/${rand_config_file_path}/config.json"
 
     yellow "启动v2ray与nginx服务"
     ## Start services
@@ -286,6 +285,8 @@ function main() {
     yum install -y epel-release && yum install -y ufw
     # only enable necessary ports
     #ufw disable && ufw allow ssh && ufw allow http && ufw allow https && ufw enable
+
+    green "客户端配置文件放置在http://${domain}/${rand_config_file_path}/config.json"
 }
 
 main
